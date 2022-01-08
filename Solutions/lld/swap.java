7b0b3c96c6f9f53341a73c9ca26d31d557577104
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-01-08 22:00:12 +0530

    Create swap.java

---
+++ Solutions/lld/swap.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+
+  static class Student{
+    int age;
+    String name;
+
+    void sayHi(){
+      System.out.println(this.name + " " + this.age);
+    }
+  }
+
+  public static void swap(Student s1, Student s2){
+    Student temp = s1;
+    s1 = s2;
+    s2 = s1;
+  }
+
+  public static void main(String[] args) {
+    Student s1 = new Student();
+    s1.age = 20;
+    s1.name = "A";
+
+    Student s2 = new Student();
+    s2.age=30;
+    s2.name = "B";
+
+    s1.sayHi();
+    s2.sayHi();
+    swap(s1, s2);
+    s1.sayHi();
+    s2.sayHi();
+
+  }
+}
