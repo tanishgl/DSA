0ad304f5a9566a462a751f55630460b598ac48ee
Tanish Goyal <73936719+tanishgl@users.noreply.github.com>
2022-01-08 22:15:11 +0530

    Create swap_dataMembers.java

---
+++ Solutions/lld/swap_dataMembers.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+
+  static class Student{
+    int age;
+    String name;
+
+    void sayHi(){
+      System.out.println(this.name + " " + this.age);
+    }
+  }
+
+  public static void swap(Student s1, Student s2){
+    int tage = s1.age;
+    s1.age = s2.age;
+    s2.age = tage;
+
+    String tname = s1.name;
+    s1.name = s2.name;
+    s2.name = tname;
+  }
+
+  public static void main(String[] args) {
+    Student s1 = new Student();
+    s1.age = 10;
+    s1.name = "A";
+
+    Student s2 = new Student();
+    s2.age=20;
+    s2.name = "B";
+
+    s1.sayHi();
+    s2.sayHi();
+    swap(s1, s2);
+    s1.sayHi();
+    s2.sayHi();
+
+  }
+}
